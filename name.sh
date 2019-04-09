11c5c136d8c62ec63ec95f9772cf4a34bc654bf4
root <hussain.abbas@rapidcompute.com>
2019-04-10 03:27:33 +0500

    new

--- name.sh
+++ name.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 jobname=$(echo $JOB_NAME | awk -F [:/] '{ print $1 }')
-ARTIFACT_URL=$(/var/lib/jenkins/jobs/$jobname/branches/$BRANCH_NAME/builds/$BUILD_NUMBER)
+ARTIFACT_URL="/var/lib/jenkins/jobs/$jobname/branches/$BRANCH_NAME/builds/$BUILD_NUMBER/archive"
 
