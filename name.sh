0f7be3888115e1031f578e93534a03542dcf33f2
root <hussain.abbas@rapidcompute.com>
2019-04-10 01:25:42 +0500

    new

--- name.sh
+++ name.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo $JOB_NAME | awk -F [:/] '{ print $1 }'
+jobname=$(echo $JOB_NAME | awk -F [:/] '{ print $1 }')
+echo "/var/lib/jenkins/jobs/$jobname/branches/$BRANCH_NAME/builds/$BUILD_NUMBER"
 
